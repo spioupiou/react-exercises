be92e54377c0215266613f17cfb4c0cd19d0a6f6
Cedrine Monnet <cedrine.monnet@gmail.com>
2022-05-23 15:57:58 +0900

    First react element

--- Ch02/wip/start/app.js
+++ Ch02/wip/start/app.js
@@ -1,6 +1,13 @@
 (function() {
   "use strict";
 
-  // Start here
+  // Element to rendered, 3 arguments, what type of elt we are creating, props we are gonna passed into this elt, and whatever should be in this elt
+  var ProductCustomizer = React.createElement(
+    'div',
+    { className: "customizer" },
+    "Product customizer will go here"
+  );
 
+  // ReactDOM.render takes two arguments: element to be rendered and where the element should go
+  ReactDOM.render(ProductCustomizer, document.getElementById('react-root'));
 })();
