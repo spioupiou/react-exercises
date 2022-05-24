e2d200ae9d1e857675d73034a336b49fb4be46a7
Cedrine Monnet <cedrine.monnet@gmail.com>
2022-05-24 15:07:21 +0900

    Added red shoe

--- Ch02/wip/start/app.js
+++ Ch02/wip/start/app.js
@@ -1,13 +1,26 @@
 (function() {
   "use strict";
 
+  const ProductImage = (props) => {
+
+    return React.createElement('img', {
+      src: "../../../assets/red.jpg",
+      alt: "Shoe"
+    });
+  }
   // Element to rendered, 3 arguments, what type of elt we are creating, props we are gonna passed into this elt, and whatever should be in this elt
-  var ProductCustomizer = React.createElement(
-    'div',
-    { className: "customizer" },
-    "Product customizer will go here"
-  );
+  const ProductCustomizer = (props) => {
+    return React.createElement(
+      'div',
+      { className: "customizer" },
+      React.createElement('div', { className: 'product-image' }, React.createElement(ProductImage))
+    );
+  };
+
 
   // ReactDOM.render takes two arguments: element to be rendered and where the element should go
-  ReactDOM.render(ProductCustomizer, document.getElementById('react-root'));
+  ReactDOM.render(
+    React.createElement(ProductCustomizer),
+    document.getElementById('react-root')
+  );
 })();
