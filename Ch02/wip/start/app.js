6637b2e0e8d819476f6776a2c0f18822f06f0c52
Cedrine Monnet <cedrine.monnet@gmail.com>
2022-05-25 12:12:49 +0900

    Added inventory data

--- Ch02/wip/start/app.js
+++ Ch02/wip/start/app.js
@@ -1,26 +1,26 @@
 (function() {
   "use strict";
 
-  const ProductImage = (props) => {
+  // Can use React.createElements with 3 arguments (type, properties and content)
+  // But better to use JSX directly, with Babel
 
-    return React.createElement('img', {
-      src: "../../../assets/red.jpg",
-      alt: "Shoe"
-    });
+  const ProductImage = (props) => {
+    return <img src="../../../assets/red.jpg" alt="Shoe" />;
   }
-  // Element to rendered, 3 arguments, what type of elt we are creating, props we are gonna passed into this elt, and whatever should be in this elt
+
+  // Element to be rendered, 3 arguments, what type of elt we are creating, props we are gonna passed into this elt, and whatever should be in this elt
   const ProductCustomizer = (props) => {
-    return React.createElement(
-      'div',
-      { className: "customizer" },
-      React.createElement('div', { className: 'product-image' }, React.createElement(ProductImage))
+    return (
+      // Can't use class but className
+      <div className="customizer">
+        <div className="product-image">
+          <ProductImage />
+        </div>
+      </div>
     );
-  };
+  }
 
 
   // ReactDOM.render takes two arguments: element to be rendered and where the element should go
-  ReactDOM.render(
-    React.createElement(ProductCustomizer),
-    document.getElementById('react-root')
-  );
+  ReactDOM.render(<ProductCustomizer />, document.getElementById('react-root'));
 })();
